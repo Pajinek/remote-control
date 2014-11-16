4cbafa3b5567159aa98565a303239e645ea31512
Pavel studenik <studenik@varhoo.cz>
2014-11-16 22:01:25 +0100

    testing on production

--- actions.py
+++ actions.py
@@ -7,19 +7,21 @@ class RemoteDevice:
     def __init__(self, hostname, username, password):
         self.password=password
         self.username=username
-        self.hostname=hostnam
+        self.hostname=hostname
         self.conn = requests.session()
 
     def call(self, path):
-        r = self.conn.post("http://%s/cgi-bin/%s" % (self.hostname, path) 
-                    auth=(self.username, self.password), data={})
+        r = self.conn.post("http://%s/cgi-bin/%s" % (self.hostname, path), auth=(self.username, self.password), data={})
+        print r
         return r
 
     def power_on(self):
         r = self.call("power_on.cgi")
+        print "power on"
 
     def power_off(self):
-        r = self.call("power_ff.cgi")
+        print "power off"
+        r = self.call("power_off.cgi")
 
     def status(self):
         r = self.call("projector_status.cgi?lang=en")
